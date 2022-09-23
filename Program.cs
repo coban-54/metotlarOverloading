ce51ba237ca19a4e2ad8c768c0982fc742e6eb5f
coban-54 <92750818+coban-54@users.noreply.github.com>
2022-09-23 18:23:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace metotlarOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi="999";
+
+
+            bool sonuc = int.TryParse(sayi,out int outSayi);
+            if (sonuc)
+            {
+                System.Console.WriteLine("Başarılı");
+                System.Console.WriteLine(outSayi);
+            }
+            else
+            System.Console.WriteLine("Başarısız");
+
+
+            Methotlar instance=new Methotlar();
+            instance.Topla(1,3,out int  result);
+            System.Console.WriteLine(result);
+
+            //Method overloading
+
+            int ifade=999;
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("HüseyinÇoban");
+            
+        }
+ 
+    }
+    class Methotlar
+    {
+        public void Topla(int a,int b,out int toplam)
+        {
+            toplam=a+b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            System.Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            System.Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            System.Console.WriteLine(veri1+veri2);
+        }
+    }
+}
